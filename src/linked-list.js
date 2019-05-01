fe7d5fa3b5ea7fdf61088c92dce890cfe7070309
ckopecky <kopecky12112@gmail.com>
2019-04-30 22:25:26 -0700

    implement removeHead in linked list

--- src/linked-list.js
+++ src/linked-list.js
@@ -24,7 +24,20 @@ class LinkedList {
   // Removes the current head node from the list, replacing it with the next element in the list
   // Returns the value of the removed node
   removeHead() {
-
+    if(this.head === this.tail){
+      this.head = null;
+      this.tail = null;
+      return null;
+    } else {
+      let oldHead = this.head;
+      this.head = this.head.next;
+      if(this.head === null){
+        this.tail = null;
+        oldHead = null;
+      }
+      console.log(oldHead);
+      return oldHead.value;
+    }
   }
   // Checks the linked list for the given value
   // Returns true if the the value is found in the list, false otherwise
@@ -33,4 +46,22 @@ class LinkedList {
   }
 }
 
-module.exports = LinkedList;
+const myList = new LinkedList;
+myList.addToTail(10);
+myList.addToTail("a");
+console.log(myList);
+
+myList.addToTail(true);
+myList.addToTail(4);
+myList.removeHead();
+myList.removeHead();
+myList.removeHead();
+myList.removeHead();
+myList.addToTail(1952);
+myList.addToTail(192);
+
+
+console.log(myList);
+
+
+// module.exports = LinkedList;
